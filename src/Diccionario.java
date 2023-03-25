475b8a254defa18fcfc6be761b1d28f626d0ce5d
Silviaillescas <silviaillesca402@gmail.com>
2023-03-24 23:17:46 -0600

    main y diccionario

--- src/Diccionario.java
+++ src/Diccionario.java
@@ -7,6 +7,96 @@
  *
  * @author silvi
  */
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Diccionario {
+ BinarySearchTree BST = new BinarySearchTree();
+    public void leer(){
+        try{
+			String linea;
+            try (BufferedReader doc = new BufferedReader(new FileReader("diccionario.txt"))) {
+                while((linea = doc.readLine()) != null ){
+                    String ingles=" ";
+                    String espanol=" ";
+                    
+                    System.out.println(linea);
+                    int contador = 0;
+                    while(contador<linea.length()){
+                        if(linea.charAt(contador)=='('){
+                            while(true){
+                                contador=contador+1;
+                                if(linea.charAt(contador)==',')
+                                    break;
+                                ingles+=linea.charAt(contador);
+                                
+                            }
+                        }
+                        if(linea.charAt(contador)==' ')
+                            while(true){
+                                contador=contador+1;
+                                if(linea.charAt(contador)==')')
+                                    break;
+                                espanol+=linea.charAt(contador);
+                                
+                            }
+                        BST.addTranslation(ingles, espanol);
+                        contador=contador+1;
+                    }
+                    
+                    //Association association = new Association(ingles,espanol);
+                    System.out.println("Se agregó" + ingles + " y" + espanol);
+                    
+                }
+            }
+		}
+		catch(IOException io){
+			System.out.println("Error, ocurrio un problema en lectura del documento u operacion");
+	
+		}
+		
+		
+	try{
+			String linea;
+			String texto=" ";
+            try (BufferedReader doc2 = new BufferedReader(new FileReader("texto.txt"))) {
+                System.out.print("Original: ");
+                while((linea = doc2.readLine()) != null ){
+                    int contador2=0;
+                    String palabra="";
+                    System.out.println(linea);
+                    
+                    while(contador2<linea.length()){
+                        
+                        
+                        if(linea.charAt(contador2)!=' ' & linea.charAt(contador2)!='.'){
+                            palabra+=linea.charAt(contador2);
+                            
+                        }
+                        
+                        else{
+                            
+                            System.out.println(palabra + " " + BST.getTranslation(palabra));
+                            //System.out.println(palabra);
+                            palabra = '*'+palabra+"*";
+                            
+                            
+                            palabra+=linea.charAt(contador2);
+                            texto+=palabra;
+                            palabra="";
+                        }
+                        contador2++;
+                    }
+                }
+            }
+			System.out.println("Traduccion: "+texto);
+	}
+		catch(IOException io){
+			System.out.println("Error, ocurrio un problema en lectura del documento u operacion");
+		}
+    }
     
+    public void creararbol(){}
 }
+
